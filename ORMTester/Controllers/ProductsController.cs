6adcbd0f36366c2d3ecf5d311b96993d4206f6ed
Enzo <159157348+nZo95@users.noreply.github.com>
2025-01-14 15:01:03 +0100

    first

---
+++ ORMTester/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ORMTester.Data;
+using ORMTester.Models;
+
+namespace ORMTester.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        private readonly AppDbContext _context;
+
+        public ProductsController(ILogger<ProductsController> logger, AppDbContext appDbContext)
+        {
+            _logger = logger;
+            _context = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ORMTester/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ORMTester.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ?Name { get; set; }
+        public string ?Type { get; set; }
+
+    }
+}
